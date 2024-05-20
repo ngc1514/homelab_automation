42fbe6b2dca3386087d71c3253845f7c48dcd19b
AidenX <aidenx58@gmail.com>
2024-05-19 21:54:03 -0400

    Added updater.sh
    Auto update script. If failed, sendmail to email address.

---
+++ updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+apt update && apt upgrade -y
+
+termRes=$(tail -50 /var/log/apt/history.log)
+result=$(echo $?)
+date=$(date)
+recipient=$("configure_email_here")
+
+if [ $result == 0 ]; then
+        echo "${date} - Updated. " >> updater.log
+else
+        echo "${date} - Update encountered an error. "  >> updater.log
+        subject="PiHole Update Failure"
+        body="${date} - Update encountered an error. Last 50 lines of APT history.log: \n ${termRes}"
+        echo -e "subject:${subject}\n\n${body}" | sendmail -t "${recipient}"
+fi
