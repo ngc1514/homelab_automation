89de3c967afe09252b05ca79402e74d31c07ff33
AidenX <aidenx58@gmail.com>
2024-05-19 22:03:11 -0400

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -5,13 +5,16 @@ apt update && apt upgrade -y
 termRes=$(tail -50 /var/log/apt/history.log)
 result=$(echo $?)
 date=$(date)
-recipient=$("configure_email_here")
+recipient="configure_email_here"
 
 if [ $result == 0 ]; then
         echo "${date} - Updated. " >> updater.log
+        subject="PiHole Update Success"
+        body="${date} - Updated successfully. Last 50 lines of APT history.log: \n ${termRes}"
 else
         echo "${date} - Update encountered an error. "  >> updater.log
         subject="PiHole Update Failure"
         body="${date} - Update encountered an error. Last 50 lines of APT history.log: \n ${termRes}"
-        echo -e "subject:${subject}\n\n${body}" | sendmail -t "${recipient}"
 fi
+
+echo -e "subject:${subject}\n\n${body}" | sendmail -t $recipient
